5ebe991d27b69fdfa444488fdea2d334bb41dc1d
Vangelis Koukis <vkoukis@arrikto.com>
2023-01-24 15:55:58 +0200

    Improve argument parsing
    Improve argument parsing by making it behave more closely to what the
    OpenSSH client expects.
    
    In a nutshell, map the "user" argument of OpenSSH to the container name
    in the pod, and support destinations formatted either as
    '[container@]pod[.namespace]' or
    'ssh://[container@]pod[.namespace][:port]'.
    
    Signed-off-by: Vangelis Koukis <vkoukis@arrikto.com>

--- kubessh.py
+++ kubessh.py
@@ -9,23 +9,58 @@ import sys
 import argparse
 
 # A wrapper to make "kubectl exec" function as ssh.
-# It's mostly syntactic sugar to convert from "ssh" syntax
+# It's mostly syntactic sugar to convert from OpenSSH's "ssh" syntax
 # to "kubectl exec" syntax.
 
 PROG = "kubessh"
 VERSION = "0.0.1"
 
 
-def parse_args():
+def parse_ssh_dest(dest, port_allowed=False):
+    """Parse a 'destination' argument in one of two supported formats.
 
-    d = ("Execute into a pod on Kubernetes with an SSH-compatible syntax."
+    Parse a 'destination' argument either as '[user@]hostname' or
+    as '[user@]hostname[:port]' depending on the value of 'port_allowed'.
+
+    """
+    if "@" in dest:
+        s = dest.split("@")
+        if len(s) != 2:
+            msg = ("Specified 'destination' must contain at most one '@'"
+                   " character.")
+            raise ValueError(msg)
+        user, hostname = s[0], s[1]
+    else:
+        user = None
+        hostname = dest
+
+    port = None
+    if port_allowed:
+        if ":" in hostname:
+            s = hostname.split(":")
+            hostname = s[0]
+            try:
+                port = int(":".join(s[1:]))
+            except ValueError:
+                raise ValueError("If specified, port must be an integer,"
+                                 " and cannot be empty.")
+
+    return user, hostname, port
+
+### FIXME: Add unittests for parse_ssh_dest()
+
+
+def parse_args():
+    """Parse command-line arguments as arguments to the OpenSSH client."""
+
+    d = ("Execute into a pod on Kubernetes with an OpenSSH-compatible syntax."
          " In other words, make 'kubectl exec' appear and function as a"
-         " replacement for the 'ssh' command-line tool, and support a"
-         " compatible syntax.\n\n"
-         "Contrary to standard 'ssh', %(prog)s will not run a login shell,"
-         " but will follow standard 'kubectl exec' semantics instead, and"
-         " will run the specified command using the container's entrypoint"
-         " instead.")
+         " replacement for the OpenSSH client [the 'ssh' command-line tool],"
+         " and support a compatible syntax.\n\n"
+         "The command to execute on the machine is optional. If omitted,"
+         " %(prog)s will attempt to run '/bin/bash', in an effort to mimic"
+         " the default behavior of 'ssh', which is to spawn a login shell"
+         " on the remote machine.")
 
     p = argparse.ArgumentParser(prog=PROG, description=d)
 
@@ -36,30 +71,45 @@ def parse_args():
     p.add_argument("-V", dest="version", action="store_true",
                    help="Output version information to stderr, and exit.")
 
-    p.add_argument("-l", metavar="POD_NAME[:CONTAINER_NAME]", dest="pod",
+    p.add_argument("-l", metavar="CONTAINER_NAME", dest="container",
                    action="store",
-                   help=("Exec into pod POD_NAME. Optionally specify"
-                         " a non-default CONTAINER_NAME."))
+                   help=("Exec into a specific container, named CONTAINER in"
+                         " the pod. If omitted, the underlying 'kubectl exec'"
+                         " will exec into the container specified via the"
+                         " 'kubectl.kubernetes.io/default-container'",
+                         " annotation, or the first container in the pod",
+                         " if no such annotation exists. %(prog) maps this"
+                         " option to the '-c' option of 'kubectl exec', see"
+                         " the output of 'kubectl help exec' for more"
+                         " details."))
 
     p.add_argument("-n", dest="pass_stdin", action="store_false",
                    help=("Prevent reading from stdin, do not pass the '-i'"
                          " option to 'kubectl exec'. Default is to use '-i',"
-                         " and pass stdin."))
+                         " and pass stdin to the container."))
 
     p.add_argument("-p", dest="port", action="store", type=int,
                    help=("This argument exists only to ensure compatibility"
                          " with 'ssh', and is ignored. %(prog)s ignores"
-                         " PORT, but it must be a valid port number,"
-                         " if specified."))
+                         " PORT, but -- if specified -- it must be a valid"
+                         " port number."))
+
     p.add_argument("destination",
                    help=("Exec into the specified destination,"
                          " which may be specified as"
-                         " pod[:container][@namespace],"
+                         " container@pod[.namespace],"
                          " or a URI of the form"
-                         " ssh://pod[:container][@namespace[:port]]."
-                         " Note: It is mandatory to specify the pod name,"
-                         " either via 'destination' or via the '-l' argument."
-                         " Note: %(prog)s will ignore 'port' if specified."))
+                         " ssh://[container@]pod[.namespace][:port]."
+                         " Note: Kubernetes does not allow namespace names to"
+                         " contain dots, but it *does* allow pod names to"
+                         " contain dots, so you have to add the namespace"
+                         " explicitly, if you wish to exec into a pod with a"
+                         " dot in its name."
+                         " Note: It's forbidden to set the container name"
+                         " both via '-l' and as part of 'destination'"
+                         " at the same time."
+                         " Note: %(prog)s will ignore 'port' if specified"
+                         " [but it must be a valid port number]."))
 
     tty_group = p.add_mutually_exclusive_group()
     tty_group.add_argument("-t", dest="alloc_tty", action="store_true",
@@ -79,51 +129,56 @@ def parse_args():
 
     args = p.parse_args()
 
-    pod = vars(args).get("pod")
+    container = vars(args).get("container")
 
-    # FIXME
-    # destination may just be the pod name, and pod is the only required
-    # argument, so the semantics don't add up very nicely compared to 'ssh'.
+    # Parse the 'destination' argument.
+    # Allowed formats are
+    # '[container@]pod[.namespace][:port]' and
+    # 'ssh://[container@]pod[.namespace][:port].
     #
+    # This aligned perfectly with SSH, which supports
+    # '[user@]hostname' and
+    # 'ssh://[user@]hostname[:port],
+    # where 'user' becomes the container name and we assume as specific
+    # format for 'hostname', using '.' to parse the namespace and pod names.
 
-    # Parse the 'destination' argument into
-    # separate 'pod', 'namespace' arguments,
-    # and check them for correctness.
-    s = args.destination.split("@")
-    if s[0] and pod is not None:
-        raise ValueError("Cannot specify pod name via 'destination' when"
-                         " using the '-l' argument.")
-        pod = s[0]
-    namespace = "@".join(s[1:])
+    # Parse destination as if it was '[user@]hostname' or
+    # ssh://[user@]hostname[:port] in the OpenSSH command line.
+    dest = args.destination
+    if dest.startswith("ssh://"):
+        user, hostname, port = parse_ssh_dest(dest[len("ssh://"):],
+                                          port_allowed=True)
+    else:
+        user, hostname, port = parse_ssh_dest(dest, port_allowed=False)
 
-    container = None
-    if ":" in pod:
-        s = pod.split(":")
-        pod = s[0]
-        container = s[1]
+    if args.port is not None and port is not None:
+        raise ValueError("Cannot specify port number via 'destination'"
+                         " when using the '-p' argument.")
 
-    port = args.port
-    if ":" in namespace:
-        if port is not None:
-            raise ValueError("Cannot specify port number via 'destination'"
-                             " when using the '-p' argument.")
-        s = namespace.split(":")
-        namespace = s[0]
-        try:
-            port = int(":".join(s[1:]))
-        except ValueError:
-            raise ValueError("If specified, port must be an integer,"
-                             " and cannot be empty.")
+    if args.container is not None and user is not None:
+        raise ValueError("Cannot specify container name via 'destination'"
+                         " when using the '-l' argument.")
 
-    if pod is None:
-        raise ValueError("You must specify a pod name,"
-                         "either via '-l' or via 'destination'.")
+    # Parse the hostname part into pod and namespace, separately
+    if "." in hostname:
+        s = hostname.split(".")
+        pod = ".".join(s[:-1])
+        namespace = s[-1]
+    else:
+        pod = hostname
+        namespace = None
+    del hostname
+
+    # Final outputs, verify correctness
+    port = args.port or port
+    container = args.container or user
+
+    if not pod:
+        raise ValueError("You must specify a pod name via 'destination'.")
     if container is not None and container == "":
         raise ValueError("If specified, container name cannot be empty.")
     if port is not None and port <= 0:
         raise ValueError("If specified, port must be a positive integer.")
-    if not namespace:
-        raise ValueError("Namespace cannot be empty.")
 
     if args.alloc_tty is None:
         args.alloc_tty = True if args.command == [] else False
